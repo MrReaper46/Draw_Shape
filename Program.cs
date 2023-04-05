3b1384e775faeca647419050005dfb3884f6f32f
MrReaper46 <129411428+MrReaper46@users.noreply.github.com>
2023-04-06 00:21:44 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw_Shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i < 5; i++)
+            {
+                for(int j=0; j < 10; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    if (i == 4)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    if (i > 0 && i < 4)
+                    {
+                        if (j == 0 || j == 8)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
