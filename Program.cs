e95d5fdd5300965120c74fa1d57231c3389ad90f
MrReaper46 <129411428+MrReaper46@users.noreply.github.com>
2023-04-06 07:57:51 +0700

    Updatè

--- Program.cs
+++ Program.cs
@@ -10,45 +10,58 @@ namespace Draw_Shape
     {
         static void Main(string[] args)
         {
-            for (int i=0; i < 5; i++)
+            int choice;
+            Console.WriteLine("Menu");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1. Print a rectangle");
+            Console.WriteLine("2. Print a square triangle");
+            Console.WriteLine("3. Print a square isosceles triangle");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("------------------------------------");
+            Console.Write("Input your choice: ");
+            choice = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------------");
+            int i, j;
+            switch (choice)
             {
-                for(int j=0; j < 10; j++)
-                {
-                    if (i == 0)
+                case 1:
+                    Console.WriteLine("Print a rectangle");
+                    for (i = 0; i < 3; i++)
                     {
-                        if (j % 2 == 0)
+                        for (j = 0; j < 10; j++)
                         {
-                            Console.Write("*");
+                            if (i == 0 || i == 2)
+                            {
+                                if (j % 2 == 0)
+                                {
+                                    Console.Write("*");
+                                }
+                                else
+                                {
+                                    Console.Write(" ");
+                                }
+                            }
+                            else
+                            {
+                                if (j == 0 || j == 8)
+                                {
+                                    Console.Write("*");
+                                }
+                                else
+                                {
+                                    Console.Write(" ");
+                                }
+                            }
                         }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
+                        Console.WriteLine();
                     }
-                    if (i == 4)
+                    break;
+                case 2:
+                    for (i = 0; i < 5; i++)
                     {
-                        if (j % 2 == 0)
-                        {
-                            Console.Write("*");
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
+
                     }
-                    if (i > 0 && i < 4)
-                    {
-                        if (j == 0 || j == 8)
-                        {
-                            Console.Write("*");
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
-                    }
-                }
-                Console.WriteLine();
+                    break;
             }
         }
     }
